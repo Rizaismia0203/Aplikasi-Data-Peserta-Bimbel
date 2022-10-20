e1c6f2654a6c5aefa74c0f4684138060bdea19cc
Rizaismia0203 <116280657+Rizaismia0203@users.noreply.github.com>
2022-10-20 22:30:39 +0700

    Delete Flutter-BookList-main_7/Flutter-BookList-main directory

--- Flutter-BookList-main_7/Flutter-BookList-main/android/app/src/main/kotlin/com/example/crud_pendataan_pasien_covid19/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.crud_pendataan_pasien_covid19
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Flutter-BookList-main_7/Flutter-BookList-main/backup/android/app/src/main/kotlin/com/example/flutter_booklist/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_booklist
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
